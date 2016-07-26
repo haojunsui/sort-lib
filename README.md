c08e5064b3c157b4070dc2bc0257bb986075f90f
Haojun Sui <shjnyr@me.com>
2016-07-26 13:54:58 -0400

    This project is going to be legen- wait for it...

---
+++ README.md
@@ -0,0 +1,56 @@
+## Sort-Lib
+
+A library of sorting algorithms in multiple languages.
+
+## Requirement
+
+### Input
+
+Binary file, which contains list of numbers which splits by comma.
+
+Example (mynumbers.txt):
+```
+4354,234325,7217,26,89235,903,573,45,146
+```
+
+### Output
+
+Binary file, which contains sorted list of numbers which splits by comma.
+
+Example (mynumbers_out.txt):
+```
+26,45,146,573,903,4354,7217,89235,234325
+```
+
+## How-To Use
+
+### C
+
+
+### C++
+
+
+### Java
+
+
+### JavaScript
+
+
+### Matlab
+
+
+### Perl
+
+
+### Python
+```
+$ python selectSort [input file]
+```
+
+### R
+
+
+### Ruby
+
+
+### Scala
---
+++ Selection Sort/selectSort.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import os
+import sys
+
+def selectSort(arr, size):
+	minPos = -1;
+	for sorted in xrange(size):
+		minPos = sorted
+		for next in xrange(sorted + 1, size):
+			if arr[minPos] > arr[next]:
+				minPos = next
+		arr[sorted], arr[minPos] = arr[minPos], arr[sorted]
+
+if __name__ == "__main__":
+	if len(sys.argv) < 2:
+		print "Too few arguments...\nUsage: python selectSort [file]"
+	else:
+		infile = open(sys.argv[1], "r")
+		fname, fext = os.path.splitext(sys.argv[1])
+		outfile = open(fname + "_out" + fext, "w")
+		nums = map(lambda x: float(x) if x.find(".") != -1 else int(x), infile.read().split(","))
+		selectSort(nums, len(nums))
+		outfile.write(",".join(map(str, nums)))
