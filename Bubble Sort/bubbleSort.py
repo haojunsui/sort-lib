925d9921706568136ec0d4bd87fcc61ad81a5fc1
Haojun Sui <shjnyr@me.com>
2016-07-26 14:16:00 -0400

    Adds Bubble Sort support.

---
+++ Bubble Sort/bubbleSort.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import os
+import sys
+
+def bubbleSort(arr, size):
+	sorted = False
+	while not sorted:
+		sorted = True;
+		for i in xrange(1, size):
+			if arr[i] < arr[i - 1]:
+				arr[i], arr[i - 1] = arr[i - 1], arr[i]
+				sorted = False;
+
+if __name__ == "__main__":
+	if len(sys.argv) < 2:
+		print "Too few arguments...\nUsage: python selectSort [file]"
+	else:
+		infile = open(sys.argv[1], "r")
+		fname, fext = os.path.splitext(sys.argv[1])
+		outfile = open(fname + "_out" + fext, "w")
+		nums = map(lambda x: float(x) if x.find(".") != -1 else int(x), infile.read().split(","))
+		bubbleSort(nums, len(nums))
+		outfile.write(",".join(map(str, nums)))
